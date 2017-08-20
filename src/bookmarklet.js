f8d4ff43a14f638c9bd97ee691d082e63f9bbd8f
Fabian Meyer <3982806+meyfa@users.noreply.github.com>
2017-08-20 14:57:36 +0200

    Implement board transform / move function

--- src/bookmarklet.js
+++ src/bookmarklet.js
@@ -60,4 +60,56 @@
         return tiles;
     };
 
+    Solver.getIterationOrder = function (dir) {
+        switch (dir) {
+            case Solver.LEFT:
+                return [1, 2, 3, 5, 6, 7, 9, 10, 11, 13, 14, 15];
+            case Solver.RIGHT:
+                return [2, 1, 0, 6, 5, 4, 10, 9, 8, 14, 13, 12];
+            case Solver.UP:
+                return [4, 8, 12, 5, 9, 13, 6, 10, 14, 7, 11, 15];
+            case Solver.DOWN:
+                return [8, 4, 0, 9, 5, 1, 10, 6, 2, 11, 7, 3];
+        }
+    };
+
+    Solver.getNewPosition = function (i, dir) {
+        var x = i % 4, y = Math.floor(i / 4);
+        switch (dir) {
+            case Solver.LEFT:
+                x = Math.max(x - 1, 0);
+                break;
+            case Solver.RIGHT:
+                x = Math.min(x + 1, 3);
+                break;
+            case Solver.UP:
+                y = Math.max(y - 1, 0);
+                break;
+            case Solver.DOWN:
+                y = Math.min(y + 1, 3);
+                break;
+        }
+        return y * 4 + x;
+    };
+
+    Solver.transform = function (board, dir) {
+        var merged = {};
+        Solver.getIterationOrder(dir).forEach(function (i) {
+            while (board[i] !== 0) {
+                var j = Solver.getNewPosition(i, dir);
+                if (board[j] === 0) {
+                    board[j] = board[i];
+                    board[i] = 0;
+                    i = j;
+                    continue;
+                } else if (j !== i && !merged[j] && board[j] === board[i]) {
+                    board[j] *= 2;
+                    board[i] = 0;
+                    merged[j] = true;
+                }
+                break;
+            }
+        });
+    };
+
 })();
--- test/test.js
+++ test/test.js
@@ -84,4 +84,75 @@ describe("Solver", function () {
 
     });
 
+    describe("#getIterationOrder()", function () {
+
+        it("should return an array", function () {
+            expect(Solver.getIterationOrder(Solver.DOWN)).to.be.an("array");
+        });
+
+        it("should not contain edge tiles", function () {
+            expect(Solver.getIterationOrder(Solver.UP)).to.not.have.members([
+                0, 1, 2, 3,
+            ]);
+        });
+
+    });
+
+    describe("#getNewPosition()", function () {
+
+        it("should return modified indices", function () {
+            expect(Solver.getNewPosition(5, Solver.LEFT)).to.equal(4);
+            expect(Solver.getNewPosition(5, Solver.RIGHT)).to.equal(6);
+            expect(Solver.getNewPosition(5, Solver.UP)).to.equal(1);
+            expect(Solver.getNewPosition(5, Solver.DOWN)).to.equal(9);
+        });
+
+        it("should limit x to 0 .. 3 inclusive", function () {
+            expect(Solver.getNewPosition(4, Solver.LEFT)).to.equal(4);
+            expect(Solver.getNewPosition(7, Solver.RIGHT)).to.equal(7);
+        });
+
+        it("should limit y to 0 .. 3 inclusive", function () {
+            expect(Solver.getNewPosition(1, Solver.UP)).to.equal(1);
+            expect(Solver.getNewPosition(13, Solver.DOWN)).to.equal(13);
+        });
+
+    });
+
+    describe("#transform()", function () {
+
+        it("should move the tiles", function () {
+            var board = [
+                0, 0, 0, 2,
+                0, 0, 2, 0,
+                0, 2, 0, 0,
+                2, 0, 0, 0,
+            ];
+            Solver.transform(board, Solver.LEFT);
+            expect(board).to.deep.equal([
+                2, 0, 0, 0,
+                2, 0, 0, 0,
+                2, 0, 0, 0,
+                2, 0, 0, 0,
+            ]);
+        });
+
+        it("should merge tiles correctly", function () {
+            var board = [
+                0, 2, 2, 2,
+                2, 2, 0, 0,
+                0, 2, 2, 4,
+                2, 0, 0, 0,
+            ];
+            Solver.transform(board, Solver.RIGHT);
+            expect(board).to.deep.equal([
+                0, 0, 2, 4,
+                0, 0, 0, 4,
+                0, 0, 4, 4,
+                0, 0, 0, 2,
+            ]);
+        });
+
+    });
+
 });
