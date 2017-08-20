85f0fc6e6c0608f4878fbe3ff77a0a617e409188
Fabian Meyer <3982806+meyfa@users.noreply.github.com>
2017-08-20 13:51:14 +0200

    Add unit tests for readTile()

--- test/test.js
+++ test/test.js
@@ -23,4 +23,39 @@ describe("Solver", function () {
 
     });
 
+    describe("#readTile", function () {
+
+        before(function () {
+            var c = document.querySelector(".tile-container");
+            c.innerHTML = [
+                '<div class="tile tile-2 tile-position-2-3">',
+                '    <div class="tile-inner">2</div>',
+                '</div>',
+                '<div class="tile tile-8 tile-position-3-4">',
+                '    <div class="tile-inner">8</div>',
+                '</div>',
+                '<div class="tile tile-16 tile-position-3-4 tile-merged">',
+                '    <div class="tile-inner">16</div>',
+                '</div>',
+            ].join("");
+        });
+
+        after(function () {
+            document.querySelector(".tile-container").innerHTML = "";
+        });
+
+        it("should detect regular tiles", function () {
+            expect(Solver.readTile(1, 2)).to.equal(2);
+        });
+
+        it("should detect merged tiles", function () {
+            expect(Solver.readTile(2, 3)).to.equal(16);
+        });
+
+        it("should return 0 for empty tiles", function () {
+            expect(Solver.readTile(1, 1)).to.equal(0);
+        });
+
+    });
+
 });
